cb90e18f3974c59a051a55cb23102390710abd28
Colin MacLeod <colinlmacleod1@gmail.com>
2016-12-15 00:30:11 -0500

    Added Example of tiles that hange color on click

--- Example 5/app/components/Prompt.js
+++ Example 5/app/components/Prompt.js
@@ -2,7 +2,7 @@ var React = require('react');
 var PropTypes = React.PropTypes;
 var transparentBg = require('../styles').transparentBg;
 
-function Prompt(props)
+function Prompt(props){
 		return (
 			<div className="jumbotron col-sm-6 col-sm-offset-3 text-center" style={transparentBg}>
 					<h1> {props.header} </h1>
@@ -27,13 +27,8 @@ function Prompt(props)
 					</div>
 				</div>
 		)
-);
+};
 
-Prompt.propTypes: {
-		header: PropTypes.string.isRequired,
-		username: PropTypes.string.isRequired,
-		onUpdateUser: PropTypes.func.isRequired,
-		onSubmitUser: PropTypes.func.isRequired
-	};
+
 module.exports = Prompt;
 
---
+++ Example/app/styles/index.js
@@ -0,0 +1,50 @@
+var styles = {
+	transparentBg: {
+		background: 'transparent'
+	},
+	red:{
+		background: 'red',
+		display: 'inline-block',
+		width: '5em',
+		height: '5em',
+		cursor: 'pointer',
+		marginTop: '0em'
+	},
+	blue:{
+		background: 'blue',
+		display: 'inline-block',
+		width: '5em',
+		height: '5em',
+		cursor: 'pointer',
+		marginTop: '0em'
+	},
+	green:{
+		background: 'green',
+		display: 'inline-block',
+		width: '5em',
+		height: '5em',
+		cursor: 'pointer',
+		marginTop: '0em'
+		},
+	yellow:{
+		background: 'yellow',
+		display: 'inline-block',
+		width: '5em',
+		height: '5em',
+		cursor: 'pointer',
+		marginTop: '0em'
+		},
+	white:{
+		background: 'white',
+		display: 'inline-block',
+		width: '5em',
+		height: '5em',
+		cursor: 'pointer',
+		marginTop: '0em'
+	}
+
+
+
+}
+
+module.exports = styles;
